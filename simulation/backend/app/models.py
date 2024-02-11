73880adf34be98d2d42eb8a9c32cf12ecc7c3422
Joanne <jungyoon.lim99@gmail.com>
2024-02-10 22:22:09 -0800

    Initial Backend work

--- simulation/backend/app/models.py
+++ simulation/backend/app/models.py
@@ -1,8 +1,22 @@
-from app import db
+from . import db
 
 # Define the Annotation model  
 class Annotation(db.model):
-    id = db.Column(db.Integer, primary_key=True)
-    text = db.Column(db.String(140))
-    timestamp = db.Column(db.DateTime, index=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    # id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), nullable=True)
+    position_x = db.Column(db.Float, nullable=False)
+    position_y = db.Column(db.Float, nullable=False)
+    position_z = db.Column(db.Float, nullable=False)
+    # timestamp = db.Column(db.DateTime, index=True)
+    # user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def to_dict(self):
+        return {
+            # 'id': self.id,
+            'name': self.modelName,
+            'position_x': self.position_x,
+            'position_y': self.position_y,
+            'position_z': self.position_z,
+            # 'timestamp': self.timestamp
+        }
+    
---
+++ simulation/backend/app/routes.py
@@ -0,0 +1,29 @@
+from flask import request, jsonify
+from . import app, db
+from .models import Annotation 
+
+"""
+Add an annotation to the database.
+This function takes no parameters and returns a JSON object representing the annotation.
+"""
+@app.route('/annotations', methods=['GET', 'POST'])
+def add_annotation():
+    data = request.get_json()
+    annotation = Annotation(
+        modelName=data['modelName'],
+        position_x=data['position_x'],
+        position_y=data['position_y'],
+        position_z=data['position_z']
+    )
+    db.session.add(annotation)
+    db.session.commit()
+    return jsonify(annotation.to_dict()), 201
+
+"""
+Get all annotations from the database.
+This function takes no parameters and returns a JSON object representing the annotations.
+"""
+@app.route('/annotations', methods=['GET'])
+def get_annotations():
+    annotations = Annotation.query.all()
+    return jsonify([annotation.to_dict() for annotation in annotations])
