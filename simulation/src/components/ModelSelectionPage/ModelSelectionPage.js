ac06f6d447595cc29f5aacef5e8af1ea8145775a
Joanne <jungyoon.lim99@gmail.com>
2024-03-09 16:31:28 -0800

    Added PropTypes validation for the setModelFile prop and Updated the handleFileSelect function to check if a file was selected before attempting to set the model file URL

--- simulation/src/components/ModelSelectionPage/ModelSelectionPage.js
+++ simulation/src/components/ModelSelectionPage/ModelSelectionPage.js
@@ -1,30 +1,38 @@
 import './ModelSelectionPage.css';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-function ModelSelectionPage({ setModelFile }){
-    const navigate = useNavigate();
+function ModelSelectionPage({ setModelFile }) {
+  const navigate = useNavigate();
 
-    // TODO: The handleFileSelect function should check if a file was selected before attempting to set the model file URL.
-    const handleFileSelect = (file) => {
-        const url = URL.createObjectURL(file);
-        setModelFile(url); 
-        navigate('/display');
-    };
+  const handleFileSelect = (file) => {
+    if (file) {
+      const url = URL.createObjectURL(file);
+      setModelFile(url);
+      navigate('/display');
+    } else {
+      console.log('No file selected');
+    }
+  };
 
-    return(
-        <div className="app-container">
-            <div className="header">
-                <h1>Select a Model</h1>
-                <input
-                    type="file"
-                    onChange={(e) => handleFileSelect(e.target.files[0])}
-                    accept=".obj"
-                    className="choose-file-button-selection"
-                />
-            </div>
-        </div>
-    );
+  return (
+    <div className="app-container">
+      <div className="header">
+        <h1>Select a Model</h1>
+        <input
+          type="file"
+          onChange={(e) => handleFileSelect(e.target.files[0])}
+          accept=".obj"
+          className="choose-file-button-selection"
+        />
+      </div>
+    </div>
+  );
 }
 
-export default ModelSelectionPage; 
+ModelSelectionPage.propTypes = {
+  setModelFile: PropTypes.func.isRequired,
+};
+
+export default ModelSelectionPage;
